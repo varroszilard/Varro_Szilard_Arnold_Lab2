5a9dcf04b800267bcc4b38912b42a73984b96a56
varroszilard <szilard_szili@yahoo.com>
2021-11-23 20:07:10 +0200

    Completare cu Lab3

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,70 @@ namespace Varro_Szilard_Arnold_Lab2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DoughnutMachine mDoughnutMachine;
+        private int mRaisedGlazed;
+        private int mRaisedSugar;
+        private int mFilledLemon;
+        private int mFilledChocolate;
+        private int mFilledVanilla;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void frmMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            mDoughnutMachine = new DoughnutMachine();
+            mDoughnutMachine.DoughnutComplete += new DoughnutMachine.DoughnutCompleteDelegate(DoughnutCompleteHandler);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void glazedToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            glazedToolStripMenuItem.IsChecked = true;
+            sugarToolStripMenuItem.IsChecked = false;
+            mDoughnutMachine.MakeDoughnuts(DoughnutType.Glazed);
+        }
+
+        private void sugarToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            glazedToolStripMenuItem.IsChecked = false;
+            sugarToolStripMenuItem.IsChecked = true;
+            mDoughnutMachine.MakeDoughnuts(DoughnutType.Sugar);
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            mDoughnutMachine.Enabled = false;
+        }
+
+        private void DoughnutCompleteHandler()
+        {
+            switch(mDoughnutMachine.Flavor)
+            {
+                case DoughnutType.Glazed:
+                    mRaisedGlazed++;
+                    txtGlazedRaised.Text = mRaisedGlazed.ToString();
+                    break;
+
+                case DoughnutType.Sugar:
+                    mRaisedSugar++;
+                    txtSugarRaised.Text = mRaisedSugar.ToString();
+                    break;
+            }
+        }
+
+        private void textBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(!(e.Key >= Key.D0 && e.Key <= Key.D9))
+            {
+                MessageBox.Show("Numai cifre se pot introduce!", "Input Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
